c867801cc409168b5403505f32476066dde51a02
David Thaler <davidthaler@gmail.com>
2020-08-15 23:09:46 -0700

    Completed all ReST verbs. Smoke tested OK.

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,41 +1,83 @@
 const express = require('express')
 
-function routes(Book){
+function routes(Book) {
     const bookRouter = express.Router()
     bookRouter.route('/books')
-    .post((req, res) => {
-        const book = new Book(req.body)
-        book.save()
-        return res.status(201).json(book)
-    })
-    .get((req, res) => {
-        const query = {}
-        if(req.query.title){
-            query.title = req.query.title
-        }
-        if(req.query.author){
-            query.author = req.query.author
-        }
-        if(req.query.genre){
-            query.genre = req.query.genre
-        }
-        Book.find(query, (err, books) => {
-            if(err){
+        .post((req, res) => {
+            const book = new Book(req.body)
+            book.save()
+            return res.status(201).json(book)
+        })
+        .get((req, res) => {
+            const query = {}
+            if (req.query.title) {
+                query.title = req.query.title
+            }
+            if (req.query.author) {
+                query.author = req.query.author
+            }
+            if (req.query.genre) {
+                query.genre = req.query.genre
+            }
+            Book.find(query, (err, books) => {
+                if (err) {
+                    return res.send(err)
+                }
+                return res.json(books)
+            })
+        })
+
+    bookRouter.use('/books/:bookId', (req, res, next) => {
+        Book.findById(req.params.bookId, (err, book) => {
+            if (err) {
                 return res.send(err)
             }
-            return res.json(books)
+            if (book) {
+                req.book = book
+                return next()
+            }
+            return res.sendStatus(404)
         })
     })
 
-bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-        Book.findById(req.params.bookId, (err, book) => {
-            if(err){
-                return res.send(err)
-            }
-            return res.json(book)
+    bookRouter.route('/books/:bookId')
+        .get((req, res) => res.json(req.book))
+        .put((req, res) => {
+            const { book } = req
+            book.title = req.body.title
+            book.author = req.body.author
+            book.genre = req.body.genre
+            book.read = req.body.read
+            book.save(err => {
+                if(err){
+                    return res.send(err)
+                }
+                return res.json(book)
+            })
         })
-    })
+        .patch((req, res) => {
+            const { book } = req
+            if(req.body._id){
+                delete req.body._id
+            }
+            Object.entries(req.body).forEach(([key, value]) => {
+                book[key] = value
+            })
+            book.save(err => {
+                if(err){
+                    return res.send(err)
+                }
+                return res.json(book)
+            })
+        })
+        .delete((req, res) => {
+            req.book.remove(err => {
+                if(err){
+                    return res.send(err)
+                }
+                res.sendStatus(204)
+            })
+        })
     return bookRouter
 }
 

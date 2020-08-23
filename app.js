5a76be53fa6a278cd81716f07865ca47dbac36e2
David Thaler <davidthaler@gmail.com>
2020-08-22 21:37:46 -0700

    misc. cleanup

--- app.js
+++ app.js
@@ -14,17 +14,12 @@ if(process.env.ENV == 'Test'){
 
 const port = process.env.PORT || 3000
 const Book = require('./models/bookModel')
-const { model } = require('./models/bookModel')
 const bookRouter = require('./routes/bookRouter')(Book)
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 
 app.use('/api', bookRouter)
-
-app.get('/', (req, res) => {
-    res.send('Welcome to my *New* PI!')
-})
 
 app.server = app.listen(port, () => {
     console.log(`Running on port ${port}`)
